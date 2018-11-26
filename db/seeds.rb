268230f14036f33cf586f6fe0f1576d75a37e490
Nadiafa <nadiafa581@gmail.com>
2018-11-26 17:30:06 +0000

    Create seeds.rb: Add some sample data

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# Create users
+mary  = User.create(name: "Mary", email: "mary@m.com", username: "mary", password: "password")
+bert = User.create(name: "Bert", email: "bert@b.com", username: "bert", password: "password")
+
+# Create items 
+Item.create(title: "Whale Watching", description: "I would like to see a whale upclose", user_id: mary.id)
+Item.create(title: "Learn pottery", description: "I would like to learn pottery", user_id: bert.id)
+
+# Use ActiveRecord to pre-associate data
+mary.items.create(title: "Mud bath", description: "I would like to take a mud bath")
+item = bert.items.build(title: "Pet Dog", description: " I would like to adopt a puppy")
+item.save
