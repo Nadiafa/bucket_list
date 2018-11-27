2e82c45639e59c86dabb14764b3c712ecd0ffdb1
Nadiafa <nadiafa581@gmail.com>
2018-11-27 13:11:12 +0000

    Update users_controller.rb: Add post '/login' route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
     erb :'users/login'
   end
 
+  post '/login' do
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else 
+      redirect "/login"
+    end
+  end 
+
   get '/signup' do
     erb :'users/signup'
   end
