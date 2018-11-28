eee657319643c1e60f95435098022d7389099b8e
Nadiafa <nadiafa581@gmail.com>
2018-11-28 09:58:39 +0000

    Update user.rb: Add activerecord validations for name, username, and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  validates :name, :username, :email, presence: true
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+  
   has_secure_password
 
   has_many :items
