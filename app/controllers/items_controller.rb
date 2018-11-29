13be3582d51fbedd3240c1d86b77f286d0a498a6
Nadiafa <nadiafa581@gmail.com>
2018-11-29 17:11:26 +0000

    Update items_controller.rb and users_controller.rb: Clean comments on glash messages

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,7 @@ class ItemsController < ApplicationController
     if logged_in?
       erb :'items/new'
     else 
-      # add flash message to say you need to be logged in to create new items
+      # flash message to say you need to be logged in to create new items
       flash[:not_logged_in] = "Please log in or sign up on order to proceed."
       redirect "/"
     end 
@@ -24,16 +24,16 @@ class ItemsController < ApplicationController
     if logged_in?
       if params[:title] != "" && params[:description] != ""
         @item = Item.create(title: params[:title], description: params[:description], user_id: current_user.id)
-        # add flash message to say you have successfully created a new item
+        # flash message to say you have successfully created a new item
         flash[:successful_create] = "You have successfully created a new Item!"
         redirect "/items/#{@item.id}"        
       else
-        # add flash message to say you have NOT created a new item + errors
+        # flash message to say you have NOT created a new item + errors
         flash[:unsuccessful_create] = "We were unable to create a new Item. Please try again."
         redirect "/new"          
       end
     else 
-      # add flash message to say you need to be logged in to create new items
+      # flash message to say you need to be logged in to create new items
       flash[:not_logged_in] = "Please log in or sign up on order to proceed."
       redirect "/"
     end 
@@ -45,12 +45,12 @@ class ItemsController < ApplicationController
       if @item.user == current_user
         erb :'items/edit'
       else 
-        # add flash message to say you can only edit your own items
+        # flash message to say you can only edit your own items
         flash[:modify_restricted] = "You can only modify your own items."
         redirect "/users/#{current_user.id}"
       end
     else 
-      # add flash message to say you need to be logged in to edit items
+      # flash message to say you need to be logged in to edit items
       flash[:not_logged_in] = "Please log in or sign up on order to proceed."
       redirect "/"
     end
@@ -61,16 +61,16 @@ class ItemsController < ApplicationController
     if logged_in?
       if @item.user == current_user
         @item.update(title: params[:title], description: params[:description])
-        # add flash message to say you have successfully edited a new item
+        # flash message to say you have successfully edited a new item
         flash[:successful_edit] = "You have successfully edite this Item!"
         redirect "/items/#{@item.id}"
       else 
-        # add flash message to say you can only edit your own items
+        # flash message to say you can only edit your own items
         flash[:modify_restricted] = "You can only modify your own items."
         redirect "/items/#{current_user.id}"
       end 
     else 
-      # add flash message to say you need to be logged in to edit items
+      # flash message to say you need to be logged in to edit items
       flash[:not_logged_in] = "Please log in or sign up on order to proceed."
       redirect "/"
     end
@@ -81,16 +81,16 @@ class ItemsController < ApplicationController
     if logged_in?
       if @item.user == current_user
         @item.destroy
-        # add flash message to say you have successfully deleted a new item
+        # flash message to say you have successfully deleted a new item
         flash[:successful_delete] = "You have successfully deleted this Item!"
         redirect "/users/#{current_user.id}"
       else
-        # add flash message to say you can only delete your own items
+        # flash message to say you can only delete your own items
         flash[:modify_restricted] = "You can only modify your own items."
         redirect "/items"
       end 
     else 
-      # add flash message to say you need to be logged in to delete items
+      # flash message to say you need to be logged in to delete items
       flash[:not_logged_in] = "Please log in or sign up on order to proceed."
       redirect "/"
     end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/login'
     else
-      # add flash message to say you are already logged in
+      # flash message to say you are already logged in
       flash[:already_logged_in] = "You were already logged in!"
       redirect "/users/#{current_user.id}"
     end 
@@ -14,11 +14,11 @@ class UsersController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      # add flash message to say you have successfully logged in
+      # flash message to say you have successfully logged in
       flash[:successful_login] = "You have successfully logged in!"
       redirect "/users/#{@user.id}"
     else 
-      # add flash message to say you have NOT logged in + errors
+      # flash message to say you have NOT logged in + errors
       flash[:unsuccessful_login] = "Your credentials were invalid. Please try again."
       redirect "/login"
     end
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/signup'
     else
-      # add flash message to say you are already logged in
+      # flash message to say you are already logged in
       flash[:already_logged_in] = "You were already logged in!"
       redirect "/users/#{current_user.id}"
     end 
@@ -38,11 +38,11 @@ class UsersController < ApplicationController
       if params[:name] != "" && params[:username] != "" && params[:email] != "" && params[:password] != ""
         @user = User.create(params)
         session[:user_id] = @user.id 
-        # add flash message to say you have successfully signed up
+        # flash message to say you have successfully signed up
         flash[:successful_signup] = "You have successfully Signed Up!"
         redirect "/users/#{@user.id}"
       else 
-        # add flash message to say you have NOT signed up + errors
+        # flash message to say you have NOT signed up + errors
         flash[:unsuccessful_signup] = "We were unable to sign you up. Please try again."
         redirect "/signup"
       end 
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
 
   get '/logout' do
     session.clear
-     # add flash message to say you have successfully logged out
+     # flash message to say you have successfully logged out
      flash[:successful_logout] = "You have successfully logged out!"
     redirect "/"
   end
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
     if logged_in?
       erb :'users/show'
     else
-      # add flash message to say you need to be logged in to proceed
+      # flash message to say you need to be logged in to proceed
       flash[:not_logged_in] = "Please log in or sign up on order to proceed."
       redirect "/"
     end 
