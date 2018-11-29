9e76aeb395bd10338b7beb56fe12633f21f701ef
Nadiafa <nadiafa581@gmail.com>
2018-11-29 18:42:50 +0000

    Update app/controllers/items_controller.rb and app/controllers/users_controller.rb: Remove flash messages comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/login'
     else
-      # flash message to say you are already logged in
       flash[:already_logged_in] = "You were already logged in!"
       redirect "/users/#{current_user.id}"
     end 
@@ -14,11 +13,9 @@ class UsersController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      # flash message to say you have successfully logged in
       flash[:successful_login] = "You have successfully logged in!"
       redirect "/users/#{@user.id}"
     else 
-      # flash message to say you have NOT logged in + errors
       flash[:unsuccessful_login] = "Your credentials were invalid. Please try again."
       redirect "/login"
     end
@@ -28,7 +25,6 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/signup'
     else
-      # flash message to say you are already logged in
       flash[:already_logged_in] = "You were already logged in!"
       redirect "/users/#{current_user.id}"
     end 
@@ -38,11 +34,9 @@ class UsersController < ApplicationController
     @user = User.new(params)
     if @user.save
       session[:user_id] = @user.id 
-      # flash message to say you have successfully signed up
       flash[:successful_signup] = "You have successfully Signed Up!"
       redirect "/users/#{@user.id}"
     else 
-      # flash message to say you have NOT signed up + errors
       flash[:unsuccessful_signup] = "We were unable to sign you up. #{@user.errors.full_messages.to_sentence}."
       redirect "/signup"
     end
@@ -50,7 +44,6 @@ class UsersController < ApplicationController
 
   get '/logout' do
     session.clear
-     # flash message to say you have successfully logged out
      flash[:successful_logout] = "You have successfully logged out!"
     redirect "/"
   end
@@ -60,7 +53,6 @@ class UsersController < ApplicationController
     if logged_in?
       erb :'users/show'
     else
-      # flash message to say you need to be logged in to proceed
       flash[:not_logged_in] = "Please log in or sign up on order to proceed."
       redirect "/"
     end 
