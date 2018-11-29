117405f3a5678444bf3122bf46b0e1844cabb3e8
Nadiafa <nadiafa581@gmail.com>
2018-11-29 11:55:52 +0000

    Update items_controller.rb: Change redirect for delete action to go to user's show page, and Add comments where flash messgaes should be added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,7 @@ class ItemsController < ApplicationController
     if logged_in?
       erb :'items/new'
     else 
+      # add flash message to say you need to be logged in to create new items
       redirect "/"
     end 
   end 
@@ -21,11 +22,14 @@ class ItemsController < ApplicationController
     if logged_in?
       if params[:title] != "" && params[:description] != ""
         @item = Item.create(title: params[:title], description: params[:description], user_id: current_user.id)
+        # add flash message to say you have successfully created a new item
         redirect "/items/#{@item.id}"        
       else
+        # add flash message to say you have NOT created a new item + errors
         redirect "/new"          
       end
     else 
+      # add flash message to say you need to be logged in to create new items
       redirect "/"
     end 
   end 
@@ -36,9 +40,11 @@ class ItemsController < ApplicationController
       if @item.user == current_user
         erb :'items/edit'
       else 
+        # add flash message to say you can only edit your own items
         redirect "/users/#{current_user.id}"
       end
     else 
+      # add flash message to say you need to be logged in to edit items
       redirect "/"
     end
   end
@@ -48,11 +54,14 @@ class ItemsController < ApplicationController
     if logged_in?
       if @item.user == current_user
         @item.update(title: params[:title], description: params[:description])
+         # add flash message to say you have successfully edited a new item
         redirect "/items/#{@item.id}"
       else 
+        # add flash message to say you can only edit your own items
         redirect "/items/#{current_user.id}"
       end 
     else 
+      # add flash message to say you need to be logged in to edit items
       redirect "/"
     end
   end 
@@ -62,11 +71,14 @@ class ItemsController < ApplicationController
     if logged_in?
       if @item.user == current_user
         @item.destroy
-        redirect "/items"
+        # add flash message to say you have successfully deleted a new item
+        redirect "/users/#{current_user.id}"
       else
+        # add flash message to say you can only delete your own items
         redirect "/items"
       end 
     else 
+      # add flash message to say you need to be logged in to delete items
       redirect "/"
     end 
   end 
