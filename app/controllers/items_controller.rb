97100044043e3f6684b6fc57a13ec7ae9fa2a818
Nadiafa <nadiafa581@gmail.com>
2018-11-30 15:12:40 +0000

    Update items_controller.rb: Refactor items_controller methods to make use of helper methods without unnecessary ifs

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,72 +7,60 @@ class ItemsController < ApplicationController
 
   get '/items/:id' do 
     set_item_found_by_id
+    
     erb :'items/show'
   end 
 
   get '/new' do
-    if logged_in?
-      erb :'items/new'
-    else 
-      not_logged_in_redirect
-    end 
+    not_logged_in_redirect
+
+    erb :'items/new'
   end 
 
   post '/items' do 
-    if logged_in?
-      @item = Item.new(title: params[:title], description: params[:description], user_id: current_user.id)
-      if @item.save
-        flash[:successful_create] = "You have successfully created a new Item!"
-        redirect "/items/#{@item.id}"        
-      else
-        flash[:unsuccessful_create] = "We were unable to create a new Item. #{@item.errors.full_messages.to_sentence}"
-        redirect "/new"          
-      end
-    else 
-      not_logged_in_redirect
-    end 
+    not_logged_in_redirect
+
+    @item = Item.new(title: params[:title], description: params[:description], user_id: current_user.id)
+    if @item.save
+      flash[:successful_create] = "You have successfully created a new Item!"
+      redirect "/items/#{@item.id}"        
+    else
+      flash[:unsuccessful_create] = "We were unable to create a new Item. #{@item.errors.full_messages.to_sentence}"
+      redirect "/new"          
+    end
   end 
 
   get '/items/:id/edit' do
     set_item_found_by_id
-    if logged_in?
-      if @item.user == current_user
-        erb :'items/edit'
-      else 
-        modify_restricted_redirect
-      end
-    else 
-      not_logged_in_redirect
-    end
+    
+    not_logged_in_redirect
+
+    modify_restricted_redirect
+
+    erb :'items/edit'
   end
 
   patch '/items/:id' do 
     set_item_found_by_id
-    if logged_in?
-      if @item.user == current_user
-        @item.update(title: params[:title], description: params[:description])
-        flash[:successful_edit] = "You have successfully edited this Item!"
-        redirect "/items/#{@item.id}"
-      else 
-        modify_restricted_redirect
-      end 
-    else 
-      not_logged_in_redirect
-    end
+    
+    not_logged_in_redirect
+
+    modify_restricted_redirect
+
+    @item.update(title: params[:title], description: params[:description])
+    flash[:successful_edit] = "You have successfully edited this Item!"
+    redirect "/items/#{@item.id}" 
   end 
 
   delete '/items/:id' do 
     set_item_found_by_id
-    if logged_in?
-      if @item.user == current_user
-        @item.destroy
-        flash[:successful_delete] = "You have successfully deleted this Item!"
-        redirect "/users/#{current_user.id}"
-      else
-        modify_restricted_redirect
-      end 
-    else 
-      not_logged_in_redirect
-    end 
+    
+    not_logged_in_redirect
+
+    modify_restricted_redirect
+
+    @item.destroy
+    flash[:successful_delete] = "You have successfully deleted this Item!"
+    redirect "/users/#{current_user.id}"
   end 
 end
